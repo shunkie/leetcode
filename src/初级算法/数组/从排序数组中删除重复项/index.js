5d3f8ec1f26218e0d0e218d8ac67f1a07c433561
shunkie <shunkie@qq.com>
2018-10-13 01:24:44 +0800

    update algorithm remove duplicate

--- src/初级算法/数组/从排序数组中删除重复项/index.js
+++ src/初级算法/数组/从排序数组中删除重复项/index.js
@@ -2,19 +2,27 @@
  * @param {number[]} nums
  * @returns {number}
  */
-const removeDuplicate = (nums) => {
+const removeDuplicate = nums => {
   let len = nums.length
-  let num = nums[0]
-  for (let i = 1; i < len; i++) {
-    if (nums[i] === num) {
-      nums.splice(i, 1)
-      i--
-      len--
-    } else {
-      num = nums[i]
-    }
+
+  if (len === 0 || len === 1) {
+    return len
   }
-  return len
+
+  // nums[0] -> nums[i]为非重复数组
+  let i = 0
+  let j = i + 1
+  while (j <= len - 1) {
+    if (nums[j] !== nums[i]) {
+      // 指向同一个元素不需要赋值
+      if (i + 1 !== j) {
+        nums[i + 1] = nums[j]
+      }
+      i++
+    }
+    j++
+  }
+  return i + 1
 }
 
 module.exports = removeDuplicate
