babdf9a1dadda1cf08cc40a93825e40da04798a5
shunkie <shunkie@qq.com>
2018-10-13 01:29:05 +0800

    add more test case for remove duplicate

--- src/初级算法/数组/从排序数组中删除重复项/test.js
+++ src/初级算法/数组/从排序数组中删除重复项/test.js
@@ -8,3 +8,15 @@ test('should return 2 when nums is [1, 1, 2]', t => {
 test('should return 5 when nums is [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]', t => {
   t.is(removeDuplicate([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]), 5)
 })
+
+test('should return 3 when nums is [0, 1, 2]', t => {
+  t.is(removeDuplicate([0, 1, 2]), 3)
+})
+
+test('should return 4 when nums is [1, 3, 3, 4, 7]', t => {
+  t.is(removeDuplicate([1, 3, 3, 4, 7]), 4)
+})
+
+test('should reutrn 8 when nums is [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 7, 7, 7]', t => {
+  t.is(removeDuplicate([0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 7, 7, 7]), 8)
+})
