2dba05eb496c377bdd37e428f69c36d33b78e983
shunkie <shunkie@qq.com>
2018-10-13 13:17:40 +0800

    rename dir name

--- src/初级算法/数组/删除排序数组中的重复项/test.js
+++ src/初级算法/数组/删除排序数组中的重复项/test.js
@@ -20,3 +20,7 @@ test('should return 4 when nums is [1, 3, 3, 4, 7]', t => {
 test('should reutrn 8 when nums is [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 7, 7, 7]', t => {
   t.is(removeDuplicate([0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 7, 7, 7]), 8)
 })
+
+test('should return 0 when nums is []', t => {
+  t.is(removeDuplicate([]), 0)
+})
