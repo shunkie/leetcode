53d90a8274d3a004a7a8695cc1d07ea506ad39ca
shunkie <shunkie@qq.com>
2018-10-14 01:46:49 +0800

    update algorithm remove element

--- src/初级算法/数组/移除元素/index.js
+++ src/初级算法/数组/移除元素/index.js
@@ -4,7 +4,19 @@
  * @returns {number}
  */
 const removeElement = (nums, val) => {
-
+  const len = nums.length
+  if (len === 0) {
+    return
+  }
+  // nums[0] -> nums[i]为非val数组
+  let i = -1
+  for (let j = 0; j < len; j++) {
+    if (nums[j] !== val) {
+      i++
+      nums[i] = nums[j]
+    }
+  }
+  return i + 1
 }
 
 module.exports = removeElement
--- src/初级算法/数组/移除元素/test.js
+++ src/初级算法/数组/移除元素/test.js
@@ -3,6 +3,18 @@ const removeElement = require('./index')
 
 test('should return 2 when nums is [3, 2, 2, 3] and val is 3', t => {
   const nums = [3, 2, 2, 3]
-  t.is(removeElement(nums, 3), 2)
+  const val = 3
+  const ret = removeElement(nums, val)
+  t.is(ret, 2)
+  nums.length = ret
   t.deepEqual(nums, [2, 2])
+})
+
+test('should return 1 when nums is [1, 2, 2] and val is 2', t => {
+  const nums = [1, 2, 2]
+  const val = 2
+  const ret = removeElement(nums, val)
+  t.is(ret, 1)
+  nums.length = ret
+  t.deepEqual(nums, [1])
 })
