3759c9c532eaeb05307904a30b9c38c8e37960ed
shunkie <shunkie@qq.com>
2018-10-14 01:28:54 +0800

    update algorithm move zeroes

--- src/初级算法/数组/移动零/index.js
+++ src/初级算法/数组/移动零/index.js
@@ -3,7 +3,21 @@
  * @returns {void} Do not return anything, modify nums in-place instead.
  */
 const moveZeroes = nums => {
-
+  const len = nums.length
+  if (len === 0) {
+    return
+  }
+  // nums[0] -> nums[i]为非0元素数组
+  let i = -1
+  for (let j = 0; j < len; j++) {
+    if (nums[j] !== 0) {
+      i++
+      nums[i] = nums[j]
+    }
+  }
+  for (let k = i + 1; k < len; k++) {
+    nums[k] = 0
+  }
 }
 
 module.exports = moveZeroes
--- src/初级算法/数组/移动零/test.js
+++ src/初级算法/数组/移动零/test.js
@@ -1,4 +1,14 @@
 const { test } = require('ava')
-const removeZeroes = require('./index')
+const moveZeroes = require('./index')
 
-test.todo('TODO')
+test('should output [1, 3, 12, 0, 0] when nums is [0, 1, 0, 3, 12]', t => {
+  const nums = [0, 1, 0, 3, 12]
+  moveZeroes(nums)
+  t.deepEqual(nums, [1, 3, 12, 0, 0])
+})
+
+test('should output [1, 0, 0] when nums is [0, 0, 1]', t => {
+  const nums = [0, 0, 1]
+  moveZeroes(nums)
+  t.deepEqual(nums, [1, 0, 0])
+})
