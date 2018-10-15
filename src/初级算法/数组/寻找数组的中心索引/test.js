44ddcfe0311654652bf564dd009f4ca8c9675511
shunkie <shunkie@qq.com>
2018-10-15 19:11:00 +0800

    fix errors in test of algorithm pivot index
    nums is not defined

--- src/初级算法/数组/寻找数组的中心索引/test.js
+++ src/初级算法/数组/寻找数组的中心索引/test.js
@@ -2,11 +2,11 @@ const { test } = require('ava')
 const pivotIndex = require('./index')
 
 test('should return 3 when nums is [1, 7, 3, 6, 5, 6]', t => {
-  t.is(pivotIndex(nums), 3)
+  t.is(pivotIndex([1, 7, 3, 6, 5, 6]), 3)
 })
 
 test('should return 0 when nums is [-1, -1, -1, 0, 1, 1]', t => {
-  t.is(pivotIndex(nums), 0)
+  t.is(pivotIndex([-1, -1, -1, 0, 1, 1]), 0)
 })
 
 test('should return -1 when nums is [1, 2, 3]', t => {
