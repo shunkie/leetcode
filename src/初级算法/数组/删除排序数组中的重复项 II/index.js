bbdf6920e3b111dc35c823fa2f1701b269843c21
shunkie <shunkie@qq.com>
2018-10-15 23:30:55 +0800

    finish algorithm remove duplicates

--- src/初级算法/数组/删除排序数组中的重复项 II/index.js
+++ src/初级算法/数组/删除排序数组中的重复项 II/index.js
@@ -3,7 +3,30 @@
  * @returns {number}
  */
 const removeDuplicates = nums => {
-
+  const len = nums.length
+  if (len <= 2) {
+    return len
+  }
+  // nums[0] -> nums[i]为元素重复不超过两次的数组
+  let i = 0
+  // count为number[i]出现的次数
+  let count = 1
+  for (let j = 1; j < len; j++) {
+    if (nums[j] === nums[i]) {
+      if (count < 2) {
+        count++
+        i++
+        nums[i] = nums[j]
+      } else {
+        count++
+      }
+    } else {
+      count = 1
+      i++
+      nums[i] = nums[j]
+    }
+  }
+  return i + 1
 }
 
 module.exports = removeDuplicates
