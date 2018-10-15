8e5c2f7daca3482a485f3d63dccee18f5c839144
shunkie <shunkie@qq.com>
2018-10-16 00:27:05 +0800

    init algorithm plus one

---
+++ src/初级算法/数组/加一/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} digits
+ * @returns {number[]}
+ */
+const plusOne = digits => {
+  return []
+}
+
+module.exports = plusOne
---
+++ src/初级算法/数组/加一/test.js
@@ -0,0 +1,14 @@
+const { test } = require('ava')
+const plusOne = require('./index')
+
+test('should return [1, 2, 4] when digits is [1, 2, 3]', t => {
+  t.deepEqual(plusOne([1, 2, 3]), [1, 2, 4])
+})
+
+test('should return [4, 3, 2, 1] when digits is [4, 3, 2, 2]', t => {
+  t.deepEqual(plusOne([4, 3, 2, 1]), [4, 3, 2, 2])
+})
+
+test('should return [1, 0] when digits is [9]', t => {
+  t.deepEqual(plusOne([9]), [1, 0])
+})
