ec61e45af1ad492b01a844c67e0483ce799f08dc
shunkie <shunkie@qq.com>
2018-10-15 14:44:33 +0800

    update test of algorithm remove duplicates

---
+++ src/初级算法/数组/删除排序数组中的重复项 II/test.js
@@ -0,0 +1,18 @@
+const { test } = require('ava')
+const removeDuplicates = require('./index')
+
+test('should return 5 when nums is [1, 1, 1, 2, 2, 3]', t => {
+  const nums = [1, 1, 1, 2, 2, 3]
+  const ret = removeDuplicates(nums)
+  t.is(ret, 5)
+  nums.length = ret
+  t.deepEqual(nums, [1, 1, 2, 2, 3])
+})
+
+test('should return 7 when nums is [0, 0, 1, 1, 1, 1, 2, 3, 3]', t => {
+  const nums = [0, 0, 1, 1, 1, 1, 2, 3, 3]
+  const ret = removeDuplicates(nums)
+  t.is(ret, 7)
+  nums.length = 7
+  t.deepEqual(nums, [0, 0, 1, 1, 2, 3, 3])
+})
