0940134775ebe61ba70aa5b993621ece4da7e483
shunkie <shunkie@qq.com>
2018-10-15 14:34:44 +0800

    update algorithm pivot index

--- src/初级算法/数组/寻找数组的中心索引/index.js
+++ src/初级算法/数组/寻找数组的中心索引/index.js
@@ -3,6 +3,23 @@
  * @returns {number}
  */
 const pivotIndex = nums => {
+  const len = nums.length
+  let sum = 0
+  for (let i = 0; i < len; i++) {
+    sum += nums[i]
+  }
+  let left = 0
+  let right = sum - left - nums[0]
+  if (left === right) {
+    return 0
+  }
+  for (let j = 1; j < len; j++) {
+    left += nums[j - 1]
+    right = sum - left - nums[j]
+    if (left === right) {
+      return j
+    }
+  }
   return -1
 }
 
