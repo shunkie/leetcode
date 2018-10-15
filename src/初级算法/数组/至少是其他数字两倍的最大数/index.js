f08a61229881d7bfc9efce67257cb59228d5e53a
shunkie <shunkie@qq.com>
2018-10-16 00:17:05 +0800

    update algorithm dominant index

--- src/初级算法/数组/至少是其他数字两倍的最大数/index.js
+++ src/初级算法/数组/至少是其他数字两倍的最大数/index.js
@@ -3,7 +3,24 @@
  * @returns {number}
  */
 const dominantIndex = nums => {
-  return -1
+  const len = nums.length
+  if (len === 1) {
+    return 0
+  }
+  let max = nums[0]
+  let index = 0
+  for (let i = 0; i < len; i++) {
+    if (max < nums[i]) {
+      max = nums[i]
+      index = i
+    }
+  }
+  for (let j = 0; j < len; j++) {
+    if (nums[j] !== max && max < 2 * nums[j]) {
+      return -1
+    }
+  }
+  return index
 }
 
 module.exports = dominantIndex
