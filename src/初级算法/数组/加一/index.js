cdf92c6e4b8f3e813ddb7e90e36cbb07f92f3c57
shunkie <shunkie@qq.com>
2018-10-16 15:09:23 +0800

    update algorithm plus one

--- src/初级算法/数组/加一/index.js
+++ src/初级算法/数组/加一/index.js
@@ -3,7 +3,30 @@
  * @returns {number[]}
  */
 const plusOne = digits => {
-  return []
+  let carry = 0
+  const len = digits.length
+  if (digits[len - 1] === 9) {
+    digits[len - 1] = 0
+    carry = 1
+    for (let i = len - 2; i >= 0; i--) {
+      if (digits[i] + carry === 10) {
+        digits[i] = 0
+        carry = 1
+      } else {
+        digits[i] += carry
+        carry = 0
+        break
+      }
+    }
+    if (carry === 1) {
+      digits.unshift(carry)
+    }
+    return digits
+  } else {
+    digits[len - 1] += 1
+    carry = 0
+    return digits
+  }
 }
 
 module.exports = plusOne
