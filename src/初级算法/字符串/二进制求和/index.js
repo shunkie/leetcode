f759ed9b46b72195843b3ef409f273cc1bb377d4
shunkie <shunkie@qq.com>
2018-10-17 00:00:05 +0800

    update algorithm add binary

--- src/初级算法/字符串/二进制求和/index.js
+++ src/初级算法/字符串/二进制求和/index.js
@@ -4,7 +4,19 @@
  * @returns {string}
  */
 const addBinary = (a, b) => {
-  return ''
+  let i = a.length - 1
+  let j = b.length - 1
+  let carry = 0
+  let res = ''
+  let m, n, sum
+  while (i >= 0 || j >= 0) {
+    m = i >= 0 ? a[i--] - 0 : 0
+    n = j >= 0 ? b[j--] - 0 : 0
+    sum = m + n + carry
+    res = sum % 2 + res
+    carry = Number.parseInt(sum / 2)
+  }
+  return carry === 1 ? '1' + res : res
 }
 
 module.exports = addBinary
