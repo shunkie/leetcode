4bef5176909bbed6226007e9ee76578e2b7d8363
shunkie <shunkie@qq.com>
2018-10-16 17:50:30 +0800

    init algorithm reverse string

---
+++ src/初级算法/字符串/反转字符串/test.js
@@ -0,0 +1,10 @@
+const { test } = require('ava')
+const reverseString = require('./index')
+
+test('should return "olleh" when str is "hello"', t => {
+  t.is(reverseString('hello'), 'olleh')
+})
+
+test('should return "amanaP :lanac a ,nalp a ,nam A" when str is "A man, a plan, a canal: Panama"', t => {
+  t.is(reverseString('A man, a plan, a canal: Panama'), 'amanaP :lanac a ,nalp a ,nam A')
+})
