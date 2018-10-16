4248ebaff057c31bc5ab4334b93b7f294820ec67
shunkie <shunkie@qq.com>
2018-10-16 21:41:33 +0800

    update algorithm reverse string

--- src/初级算法/字符串/反转字符串/index.js
+++ src/初级算法/字符串/反转字符串/index.js
@@ -3,7 +3,22 @@
  * @returns {string}
  */
 const reverseString = str => {
-  return ''
+  const len = str.length
+  if (len <= 1) {
+    return str
+  }
+  const arr = str.split('')
+  let i = 0
+  let j = len - 1
+  let temp
+  while (i < j) {
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp  
+    i++
+    j--
+  }
+  return arr.join('')
 }
 
 module.exports = reverseString
