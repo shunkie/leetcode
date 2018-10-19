a04e87bee79c92be3626c47991a0064680c6b81a
shunkie <shunkie@qq.com>
2018-10-20 03:17:46 +0800

    add test case of algorithm single number

--- src/初级算法/数组/只出现一次的数字/test.js
+++ src/初级算法/数组/只出现一次的数字/test.js
@@ -1,4 +1,10 @@
 const { test } = require('ava')
 const singleNumber = require('./index')
 
-test.todo('TODO')
+test('should return 1 when nums is [2, 2, 1]', t => {
+  t.is(singleNumber([2, 2, 1]), 1)
+})
+
+test('should return 4 when nums is [4, 1, 2, 1, 2]', t => {
+  t.is(singleNumber([4, 1, 2, 1, 2]), 4)
+})
