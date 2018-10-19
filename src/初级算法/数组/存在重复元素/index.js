b22af4b027deaed9cce919e49eb228fd6ad93808
shunkie <shunkie@qq.com>
2018-10-19 22:20:00 +0800

    update algorithm contains duplicate

--- src/初级算法/数组/存在重复元素/index.js
+++ src/初级算法/数组/存在重复元素/index.js
@@ -3,5 +3,13 @@
  * @returns {boolean}
  */
 const containsDuplicate = nums => {
+  nums.sort((a, b) => a - b) // in-place
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      return true
+    }
+  }
   return false
 }
+
+module.exports = containsDuplicate
