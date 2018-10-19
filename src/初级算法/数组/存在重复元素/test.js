5452fd6e3c1906781e61369b893e148907aac71c
shunkie <shunkie@qq.com>
2018-10-19 22:11:26 +0800

    add test case of algorithm contains duplicate

--- src/初级算法/数组/存在重复元素/test.js
+++ src/初级算法/数组/存在重复元素/test.js
@@ -1,4 +1,14 @@
 const { test } = require('ava')
 const containsDuplicate = require('./index')
 
-test.todo('TODO')
+test('should return true when nums is [1, 2, 3, 1]', t => {
+  t.true(containsDuplicate([1, 2, 3, 1]))
+})
+
+test('should return false when nums is [1, 2, 3, 4]', t => {
+  t.false(containsDuplicate([1, 2, 3, 4]))
+})
+
+test('should return true when nums is [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]', t => {
+  t.true(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+})
