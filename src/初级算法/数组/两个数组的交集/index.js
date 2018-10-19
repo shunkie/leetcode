9c2116c3a09f9e2bce259b0c28ca2baf1afcb2d7
shunkie <shunkie@qq.com>
2018-10-20 03:25:51 +0800

    init algorithm intersection

---
+++ src/初级算法/数组/两个数组的交集/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @returns {number[]}
+ */
+const intersection = (nums1, nums2) => {
+  return []
+}
+
+module.exports = intersection
---
+++ src/初级算法/数组/两个数组的交集/test.js
@@ -0,0 +1,4 @@
+const { test } = require('ava')
+const intersection = require('./index')
+
+test.todo('TODO')
