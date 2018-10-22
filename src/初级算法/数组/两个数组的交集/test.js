301e203b987bdaf1b9590b85b038103f7a80f116
shunkie <shunkie@qq.com>
2018-10-22 23:40:18 +0800

    add teset case of algorithm intersection

--- src/初级算法/数组/两个数组的交集/test.js
+++ src/初级算法/数组/两个数组的交集/test.js
@@ -1,4 +1,19 @@
 const { test } = require('ava')
 const intersection = require('./index')
 
-test.todo('TODO')
+test('should return [2] when nums1 is [1, 2, 2, 1] and nums2 is [2, 2]', t => {
+  const nums1 = [1, 2, 2, 1]
+  const nums2 = [2, 2]
+  const res = intersection(nums1, nums2)
+  t.is(res.length, 1)
+  t.not(res.indexOf(2), -1)
+})
+
+test('should return [9, 4] when nums1 is [4, 9, 5] and nums2 is [9, 4, 9, 8, 4]', t => {
+  const nums1 = [9, 4]
+  const nums2 = [9, 4, 9, 8, 4]
+  const res = intersection(nums1, nums2)
+  t.is(res.length, 2)
+  t.not(res.indexOf(9), -1)
+  t.not(res.indexOf(4), -1)
+})
