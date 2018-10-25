ae93f6bf2e95a8adb338efded834d5bd5204b56c
shunkie <shunkie@qq.com>
2018-10-25 23:53:31 +0800

    add test case of algorithm get sum

--- src/初级算法/数学/两整数之和/test.js
+++ src/初级算法/数学/两整数之和/test.js
@@ -1,4 +1,10 @@
 const { test } = require('ava')
 const getSum = require('./index')
 
-test.todo('TODO')
+test('should return 3 when a is 1 and b is 2', t => {
+  t.is(getSum(1, 2), 3)
+})
+
+test('should return 1 when a is -2 and b is 3', t => {
+  t.is(getSum(-2, 3), 1)
+})
