97a38c27896d4ef10862ad9e0f7e1b1da683e920
shunkie <shunkie@qq.com>
2018-10-25 23:48:22 +0800

    init algorithm get sum

---
+++ src/初级算法/数学/两整数之和/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+const getSum = (a, b) => {
+  return 0
+}
+
+module.exports = getSum
---
+++ src/初级算法/数学/两整数之和/test.js
@@ -0,0 +1,4 @@
+const { test } = require('ava')
+const getSum = require('./index')
+
+test.todo('TODO')
