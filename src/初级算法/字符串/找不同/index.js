52acc116175a3a203ab0626532f5cb762cf0a84e
shunkie <shunkie@qq.com>
2018-10-25 23:41:09 +0800

    update algorithm find the difference

--- src/初级算法/字符串/找不同/index.js
+++ src/初级算法/字符串/找不同/index.js
@@ -4,7 +4,12 @@
  * @returns {string}
  */
 const findTheDifference = (s, t) => {
-  return ''
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    res = res ^ s.charCodeAt(i) ^ t.charCodeAt(i)
+  }
+  res = res ^ t.charCodeAt(t.length - 1)
+  return String.fromCharCode(res)
 }
 
 module.exports = findTheDifference
