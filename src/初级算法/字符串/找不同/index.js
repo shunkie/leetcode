05025c399012c82a000f80e84372513a09d61a4d
shunkie <shunkie@qq.com>
2018-10-25 23:26:35 +0800

    init algorithm find the difference

---
+++ src/初级算法/字符串/找不同/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @returns {string}
+ */
+const findTheDifference = (s, t) => {
+  return ''
+}
+
+module.exports = findTheDifference
---
+++ src/初级算法/字符串/找不同/test.js
@@ -0,0 +1,4 @@
+const { test } = require('ava')
+const findTheDifference = require('./index')
+
+test.todo('TODO')
