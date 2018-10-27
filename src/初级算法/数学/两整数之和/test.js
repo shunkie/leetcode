1d0eda4a3dca9177f784b03df6e611b2de2ca85e
shunkie <shunkie@qq.com>
2018-10-27 23:37:08 +0800

    add test case of algorithm get sum

--- src/初级算法/数学/两整数之和/test.js
+++ src/初级算法/数学/两整数之和/test.js
@@ -8,3 +8,11 @@ test('should return 3 when a is 1 and b is 2', t => {
 test('should return 1 when a is -2 and b is 3', t => {
   t.is(getSum(-2, 3), 1)
 })
+
+test('should return 198 when a is 99 and b is 99', t => {
+  t.is(getSum(99, 99), 198)
+})
+
+test('should return 0 when a is -99 and b is 99', t => {
+  t.is(getSum(-99, 99), 0)
+})
