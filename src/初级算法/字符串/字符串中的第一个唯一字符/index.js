b7252f86e937d1e054bcbd343dce531f9b58b411
shunkie <shunkie@qq.com>
2018-10-29 00:19:30 +0800

    update algorithm first uniq char

--- src/初级算法/字符串/字符串中的第一个唯一字符/index.js
+++ src/初级算法/字符串/字符串中的第一个唯一字符/index.js
@@ -3,6 +3,19 @@
  * @returns {number}
  */
 const firstUniqChar = s => {
+  const map = new Map()
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1)
+    } else {
+      map.set(s[i], 1)
+    }
+  }
+  for (let j = 0; j < s.length; j++) {
+    if (map.get(s[j]) === 1) {
+      return j
+    }
+  }
   return -1
 }
 
