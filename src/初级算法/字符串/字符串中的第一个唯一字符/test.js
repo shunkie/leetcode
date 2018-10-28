09049cf1fab1c196281c732477f0215a9ec994f3
shunkie <shunkie@qq.com>
2018-10-29 00:21:58 +0800

    add test case of algorithm first uniq char

--- src/初级算法/字符串/字符串中的第一个唯一字符/test.js
+++ src/初级算法/字符串/字符串中的第一个唯一字符/test.js
@@ -1,4 +1,14 @@
 const { test } = require('ava')
 const firstUniqChar = require('./index')
 
-test.todo('TODO')
+test('should return 0 when s is leetcode', t => {
+  t.is(firstUniqChar('leetcode'), 0)
+})
+
+test('should return 0 when s is leetcode', t => {
+  t.is(firstUniqChar('loveleetcode'), 2)
+})
+
+test('should return -1 when s is aabbcc', t => {
+  t.is(firstUniqChar('aabbcc'), -1)
+})
