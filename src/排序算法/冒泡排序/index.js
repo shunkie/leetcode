d1180c2bfdea86a0ad2f806ed9f2339359e6a0ec
shunkie <shunkie@qq.com>
2018-10-30 21:37:37 +0800

    update algorithm bubble sort

--- src/排序算法/冒泡排序/index.js
+++ src/排序算法/冒泡排序/index.js
@@ -6,10 +6,10 @@ const bubbleSort = nums => {
   let temp
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = 0; j < nums.length - 1 - i; j++) {
-      if (nums[i] > nums[i + 1]) {
-        temp = nums[i]
-        nums[i] = nums[i + 1]
-        nums[i + 1] = temp
+      if (nums[j] > nums[j + 1]) {
+        temp = nums[j]
+        nums[j] = nums[j + 1]
+        nums[j + 1] = temp
       }
     }
   }
