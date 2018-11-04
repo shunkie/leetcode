ca63d4ad38baa77a4951980366b47302aa0b8250
shunkie <shunkie@qq.com>
2018-11-04 13:17:57 +0800

    add test case of algorithm selection sort

--- src/排序算法/选择排序/test.js
+++ src/排序算法/选择排序/test.js
@@ -6,3 +6,9 @@ test('should return [1, 2, 3] when nums is [3, 2, 1]', t => {
   selectionSort(nums)
   t.deepEqual(nums, [1, 2, 3])
 })
+
+test('should return [1, 2, 3, 4, 5, 6, 7, 8, 9] when nums is [9, 8, 7, 6, 5, 4, 3, 2, 1]', t => {
+  const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+  selectionSort(nums)
+  t.deepEqual(nums, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+})
