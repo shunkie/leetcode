76afe0aa24b17a46bdd3b7af389f699194826ce8
shunkie <shunkie@qq.com>
2019-03-17 02:28:10 +0800

    finish reverse words

--- src/初级算法/字符串/反转字符串中的单词 III/index.js
+++ src/初级算法/字符串/反转字符串中的单词 III/index.js
@@ -3,7 +3,9 @@
  * @returns {string}
  */
 const reverseWords = (str) => {
-  return str
+  return str.split(' ').map(value => {
+    return value.split('').reverse().join('')
+  }).join(' ')
 }
 
 module.exports = reverseWords
--- src/初级算法/字符串/反转字符串中的单词 III/test.js
+++ src/初级算法/字符串/反转字符串中的单词 III/test.js
@@ -3,5 +3,5 @@ const reverseWords = require('./index')
 
 test(`should return [s'teL ekat edoCteeL tsetnoc] when str is [Let's take LeetCode contest]`, t => {
   let str = `Let's take LeetCode contest`
-  t.is(reverseWords(str), `Let's take LeetCode contest`)
+  t.is(reverseWords(str), `s'teL ekat edoCteeL tsetnoc`)
 })
