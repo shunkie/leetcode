c9e20093d743724c9e2b1d6dc73acb7a2e18ae38
shunkie <shunkie@qq.com>
2019-03-17 02:25:44 +0800

    init algorithm reverse words

---
+++ src/初级算法/字符串/反转字符串中的单词 III/test.js
@@ -0,0 +1,7 @@
+const test = require('ava').default
+const reverseWords = require('./index')
+
+test(`should return [s'teL ekat edoCteeL tsetnoc] when str is [Let's take LeetCode contest]`, t => {
+  let str = `Let's take LeetCode contest`
+  t.is(reverseWords(str), `Let's take LeetCode contest`)
+})
